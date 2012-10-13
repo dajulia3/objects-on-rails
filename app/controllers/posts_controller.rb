5c25f4c63e01db0d3998200210ed64815dc25de9
David Julia <dajulia3@gmail.com>
2012-10-12 18:16:48 -0600

    Added flash notice and corresponding twitter bootstrap alert styles to application layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
+  def create
+    @post = @blog.new_post(params[:post])
+    @post.publish
+    redirect_to root_path, notice: "Post added!"
+  end
+
   def new
     @post = @blog.new_post
   end
